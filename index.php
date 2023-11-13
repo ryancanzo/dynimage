9eef22cc17c4d84371797669f32d5e3035614530
ryancanzo <ryancanzo@yahoo.com>
2023-11-13 11:31:59 +0000

    CloudFlare functions added to parse options

--- index.php
+++ index.php
@@ -12,8 +12,61 @@ if ($dyn_size > $max_size) $dyn_size = $max_size;
 
 // Parse the request
 $uri = $_SERVER['REQUEST_URI'];
-$uri = reset(explode('?', $uri)); // Remove query string
 $file_extension = strtolower(pathinfo($uri, PATHINFO_EXTENSION));
+$file_extension = reset(explode('?', $file_extension)); // Remove query string
+
+// Parse CloudFlare's image resizing format
+function parse_cf_options($uri) {
+    // Pattern to extract the part of the URI that contains the options
+    $pattern = '/\/cdn-cgi\/image\/([^\/]+)/';
+    $matches = [];
+
+    if (preg_match($pattern, $uri, $matches)) {
+        // Extract the options part
+        $optionsPart = $matches[1];
+
+        // Split the options by comma
+        $optionsList = explode(',', $optionsPart);
+
+        $options = [];
+        foreach ($optionsList as $option) {
+            // Split each option by '=' to get key and value
+            list($key, $value) = explode('=', $option);
+
+            // Add the option to the result array
+            $options[$key] = $value;
+        }
+
+        return $options;
+    }
+
+    // Return empty array if no options are found
+    return [];
+}
+
+// Removes the CloudFlare options string from a URL
+function remove_cf_portion($url) {
+    $pattern = '/\/cdn-cgi\/image\/[^\/]+\//';
+    $cleaned_url = preg_replace($pattern, '/', $url);
+    return $cleaned_url;
+}
+
+$cf_options = parse_cf_options($uri);
+if (count($cf_options) > 0) {
+    $uri_query_string = "";
+    $query_parts = [];
+    // Set the size and quality settings with the CloudFlare options
+    if (isset($cf_options['width'])) {
+        $dyn_size = intval($cf_options['width']);
+        $_GET["s"] = $dyn_size;
+    }
+    if (isset($cf_options['quality'])) {
+        $dyn_quality = intval($cf_options['quality']);
+        $_GET["q"] = $dyn_quality;
+    }
+    // Remove the CloudFlare options from the URI so that the original file can be found
+    $uri = remove_cf_portion($uri);
+}
 
 // Processed files are saved in different location
 $prepend_path = "";
@@ -26,45 +79,26 @@ if (in_array($file_extension, ['jpg', 'jpeg', 'png'])) {
 $local_file_path = $local_path . $prepend_path . $uri;
 $remote_file_path = $origin_url . $uri;
 
-// Set the local directory and create it if it doesn't exist
-$directory = dirname($local_file_path);
-if (!is_dir($directory)) {
-    // Attempt to create the directory
-    if (!mkdir($directory, 0755, true)) {
-        die('Failed to create directories...');
-    }
-}
-
-// Function resizes an image proportionally based on the max size in any direction
-function resize_image ($image, $size) {
-    // Get pixel dims of original image
-    $width = imagesx($image); 
-    $height = imagesy($image);
-
-    // Determine aspect ratio of image
-    $aspect_ratio = $width/$height;
-
-    // Determine if image is landscape or portrait
-    $new_width = ($width > $height) ? $size : floor($size*$aspect_ratio);
-    $new_height = ($width > $height) ? floor($new_width/$aspect_ratio) : $size;
-
-    // Create a new image
-    $new_image = imagecreatetruecolor($new_width, $new_height);
-    imagecopyresampled($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
-    
-    // Return the new image
-    return $new_image;
-}
-
-// Check if the file exists locally and process if necessary
+// Check if the file exists locally
 if (!file_exists($local_file_path)) {
+    // Download the file from the origin server
     $file_content = file_get_contents($remote_file_path);
     if ($file_content === false) {
         header("HTTP/1.0 404 Not Found");
         exit('File not found');
     }
 
-    if ($file_extension == 'jpg' || $file_extension == 'jpeg' || $file_extension == 'png') {
+    // Set the local directory and create it if it doesn't exist
+    $directory = dirname($local_file_path);
+    if (!is_dir($directory)) {
+        // Attempt to create the directory
+        if (!mkdir($directory, 0755, true)) {
+            die('Failed to create directories...');
+        }
+    }
+
+    // If the file is an image and it's to be resized or compressed
+    if (in_array($file_extension, ['jpg', 'jpeg', 'png']) && (isset($_GET["s"]) || isset($_GET["q"]))) {
         $image = imagecreatefromstring($file_content);
         if (!$image) {
             die('Invalid image file');
@@ -72,6 +106,26 @@ if (!file_exists($local_file_path)) {
 
         // Apply resizing
         if (isset($dyn_size)) {
+            // Function resizes an image proportionally based on the max size in any direction
+            function resize_image ($image, $size) {
+                // Get pixel dims of original image
+                $width = imagesx($image); 
+                $height = imagesy($image);
+
+                // Determine aspect ratio of image
+                $aspect_ratio = $width/$height;
+
+                // Determine if image is landscape or portrait
+                $new_width = ($width > $height) ? $size : floor($size*$aspect_ratio);
+                $new_height = ($width > $height) ? floor($new_width/$aspect_ratio) : $size;
+
+                // Create a new image
+                $new_image = imagecreatetruecolor($new_width, $new_height);
+                imagecopyresampled($new_image, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
+                
+                // Return the new image
+                return $new_image;
+            }
             $image = resize_image($image, $dyn_size);
         }
 
@@ -86,7 +140,7 @@ if (!file_exists($local_file_path)) {
                 break;
         }
         imagedestroy($image);
-        // Clear file status cache, without this the image won't display on first attempt
+        // Clear file status cache, without this the image won't display on first load
         clearstatcache(true, $local_file_path);
     } else {
         // For non-image files or SVG, just save the file as is
@@ -95,9 +149,7 @@ if (!file_exists($local_file_path)) {
 }
 
 // Set CORS headers
-/*
-    Add CORS logic here
-*/
+header("Access-Control-Allow-Origin: *");
 
 // Serve the file with the correct content type
 switch ($file_extension) {
